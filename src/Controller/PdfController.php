be87236c2750b0aa4d42dcf25ab3ce1a084069ce
elitecolors <saidaniahmed125@gmail.com>
2019-06-09 00:26:38 +0200

    add 2 button pdf

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -75,5 +75,61 @@ class PdfController extends Controller
         ]);
     }
 
+    /**
+     * @Route("/print2", defaults={}, name="print2")
+     */
+    public function print2($id)
+    {
+        // get user
+        $repo=$this->getDoctrine()->getRepository('App:ListTable');
+
+        $user = $this->getUser();
+
+        $repoModel=$this->getDoctrine()->getRepository('App:Model');
+
+        $listable=$repo->findByUser($user->getId(),$id);
+
+        $model=$repoModel->find($id);
+
+        $formation=array();
+        if(!empty($listable)){
+            foreach ($listable as $row){
+                $formation[$row->getName()]['realise']=$row->getRealise();
+                $formation[$row->getName()]['value']=$row->getValue();
+
+            }
+        }
+
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('default/print_2_html.html.twig', [
+            'formation' => $formation,
+            'user'=>$user,
+            'model'=>$model
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+
 
 }
